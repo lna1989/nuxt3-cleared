8ba0db8ae5502203b78865eb24553935719fae77
Nikita Loshchenin <nikita@loshchenin.ru>
2023-05-10 17:18:30 +0400

    youtrack JetBrains TS1109 error

---
+++ components/tserror.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Import this file to the page/error.vue get TS1109: Expression expected error</h1>
+</template>
---
+++ pages/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <AppTserror/>
+</template>
+
+
+<script lang="ts" setup>
+import AppTserror from '~/components/tserror.vue'
+</script>
